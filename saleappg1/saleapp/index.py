caa476ed417cfa67b5a0e52665bd770ebd915434
hau.nt <hau.nt@ou.edu.vn>
2025-10-24 15:55:51 +0700

    Week 2

--- saleappg1/saleapp/index.py
+++ saleappg1/saleapp/index.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request
+import math
+from flask import render_template, request
 import dao
+from saleapp import app
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("cate_id")
     prods = dao.load_products(q=q, cate_id=cate_id)
-    return render_template("index.html", prods=prods)
+    pages = math.ceil(dao.count_product()/3)
+    return render_template("index.html", prods=prods, pages=pages)
 
 @app.route("/products/<int:id>")
 def details(id):
