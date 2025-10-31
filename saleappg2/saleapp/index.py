48e466568bbffa5513132965cb3167424c181415
hau.nt <hau.nt@ou.edu.vn>
2025-10-31 16:32:03 +0700

    Week 2

--- saleappg2/saleapp/index.py
+++ saleappg2/saleapp/index.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
 import dao
+from saleapp import app
+import math
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("cate_id")
-    prods = dao.load_products(q=q, cate_id=cate_id)
-    return render_template("index.html", prods=prods)
+    page = request.args.get("page")
+    prods = dao.load_products(q=q, cate_id=cate_id, page=page)
+    pages = math.ceil(dao.count_product()/app.config["PAGE_SIZE"])
+    return render_template("index.html", prods=prods, pages=pages)
 
 @app.route("/products/<int:id>")
 def details(id):
@@ -20,6 +23,10 @@ def common_attribute():
     return {
         "cates": dao.load_categories()
     }
+
+@app.route("/login")
+def login_my_user():
+    return render_template("login.html")
 
 if __name__== "__main__":
     with app.app_context():
---
+++ saleappg2/saleapp/models.py
@@ -0,0 +1,38 @@
+import json
+
+from saleapp import db, app
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Category(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(150), unique=True, nullable=False)
+    products = relationship('Product', backref="category", lazy=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(150), unique=True, nullable=False)
+    price = Column(Float, default=0.0)
+    image = Column(String(300), default="https://res.cloudinary.com/dy1unykph/image/upload/v1740037805/apple-iphone-16-pro-natural-titanium_lcnlu2.webp")
+    cate_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+if __name__=="__main__":
+    with app.app_context():
+        # db.create_all()
+        c1 = Category(name="Laptop")
+        c2 = Category(name="Mobile")
+        c3 = Category(name="Tablet")
+        print(c1)
+        # db.session.add_all([c1,c2,c3])
+
+        with open("data/product.json", encoding="utf-8") as f:
+            products = json.load(f)
+
+            for p in products:
+                db.session.add(Product(**p))
+
+        db.session.commit()
